0d6801c5df936aeb49efd097022254e96d69fd38
Luke Snowden <luke@purposemedia.co.uk>
2015-03-01 13:01:51 +0000

    update

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -526,10 +526,8 @@
 				var _body = $('body');
 				_body.removeClass( removeClass = 'active-slide-' + (before+1) );
 				_body.addClass( 'active-slide-' + (after+1) );
-
 				$( options.selector, body ).eq( before ).removeClass( 'active-slide' );
 				$( options.selector, body ).eq( after ).addClass( 'active-slide' );
-
 				if( options.nthClasses ) {
 					_body.removeClass( 'active-nth-slide-' + (( before % options.nthClasses )+1) );
 					_body.addClass( 'active-nth-slide-' + (( after % options.nthClasses )+1) );
